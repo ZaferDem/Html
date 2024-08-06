0148c8091ed51a9e4dcda8632b636c000d629231
ZaferDem <146586182+ZaferDem@users.noreply.github.com>
2024-08-06 18:32:54 +0300

    Html .py

---
+++ .py
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    #baş kodu
+    <title>Website</title>#başlık
+    <link rel="stylesheet" href="Padisahlar.css">
+           #?               #?
+</head>
+<style>
+    #stil kodu
+    img {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      #ortalama kodu
+    }
+</style>
+<body>
+    #vücud kodu
+    <table border=1>
+        # table tablo demek
+        <tr>
+            <td colspan="6"><img src="PadisahResimleri/Osman.jpeg" alt="Osman Paşa Osmanlı" style="width:50%;">
+          #colspan satır ları birleştirir  
+        </tr>
+        <tr>
+            <td colspan="6"><h1>Osman Paşa(1. padişah)</h1></td>
+            #colspan satır ları birleştirir
+        </tr>
+        <tr>
+        #sütun demek tr
+            <td><img src="PadisahResimleri/Orhan.jpg" alt="Orhan Paşa Osmanlı" class="center"></td>
+            #satır demek td
+            <td>Orhan(2.padişah)</td>
+            <td><img src="PadisahResimleri/Mehmed.jpg" alt="Mehmed Paşa Osmanlı" class="center"></td>
+            <td>I. Mehmed(5.padişah)</td>
+            <td><img src="PadisahResimleri/Beyazid2.jpg" alt="II. Bayezid" class="center"></td>
+            <td>II. Beyazid(8.padişah)</td>
+        </tr>
+        <tr>
+            <td><img src="PadisahResimleri/Murad.jpg" alt="Murad Paşa Osmanlı" class="center"></td>
+            <td>I. Murad(3.padişah)</td>
+            <td><img src="PadisahResimleri/Murad2.jpg" alt="Murad2 Paşa Osmanlı" class="center"></td>
+            <td>II. Murad(6.padişah)</td>
+            <td><img src="PadisahResimleri/Selim.jpg" alt="1.Selim" class="center"></td>
+            <td>I. Selim(9.padişah)</td>
+        </tr>
+        <tr>
+            <td><img src="PadisahResimleri/Beyazid.jpg" alt="Bayezid Paşa Osmanlı" class="center"></td>
+            <td>I. Bayezid(4.padişah)</td>
+            <td><img src="PadisahResimleri/Mehmed2.jpg" alt="Mehmed2 Paşa Osmanlı" class="center"></td>
+            <td>II. Mehmed(7.padişah)</td>
+            <td><img src="PadisahResimleri/Süliman2.jpg" alt="1.Süleyman" class="center"></td></td>
+            <td>I. Süleyman(10.padişah)</td>
+        </tr>
+    </table>
+     
+</body>
+</html>
